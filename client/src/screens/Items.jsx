6ab892b3508cccacc313d25cce9907403f74fa38
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-03-17 11:40:20 -0400

    removed see more and added link to item edit

--- client/src/screens/Items.jsx
+++ client/src/screens/Items.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import Layout from '../components/shared/Layout'
 
 export default function Items(props) {
   const { history, match, user, items } = props
-  const renderButton = id => {
-    if (user) {
-      return (
-        <button onClick={() => history.push(`${match.url}/${id}`)}>
-          See More
-                </button>
-      )
-    } else {
-      return null
-    }
-  }
+  // const renderButton = id => {
+  //   if (user) {
+  //     return (
+  //       <button onClick={() => history.push(`${match.url}/${id}`)}>
+  //         See More
+  //       </button>
+  //     )
+  //   } else {
+  //     return null
+  //   }
+  // }
 
   const renderItems = () => {
     if (items) {
       return items.map(item => {
         return (
           <div className="item" key={item._id}>
-            <h4>{item.title}</h4>
-            {renderButton(item._id)}
+
+            <Link to="/items/:id/edit" className="editPage">
+              <h3>{item.title} - {item.link}</h3>
+            </Link>
+            {/* {renderButton(item._id)} */}
           </div>
         )
       })
@@ -33,10 +37,11 @@ export default function Items(props) {
   if (user) {
     return (
       <Layout>
-        <h4>Items</h4>
-        {!items ? <h3>No Items at this time.</h3> : null}
-        <div className="item-container">{renderItems()}</div>
-        
+        <div>
+          <h3>My Movies</h3>
+          {!items ? <h3>No Items at this time.</h3> : null}
+          <div className="item-container">{renderItems()}</div>
+        </div>
       </Layout>
     )
   } else {
